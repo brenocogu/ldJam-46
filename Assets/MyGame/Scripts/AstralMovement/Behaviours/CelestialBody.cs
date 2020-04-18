0b26208a299d1928faaa1eb8314f422ad565ee76
Breno W <brenowmf@gmail.com>
2020-04-17 23:23:57 -0300

    Added Senders and Receivers Addes celestial event senders and planet receivers script

---
+++ Assets/MyGame/Scripts/AstralMovement/Behaviours/CelestialBody.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using com.vrglass.eventcontroller;
+
+namespace Gameplay.Sky
+{
+    public enum BodyType
+    {
+        SUN,
+        MOON
+    }
+
+    public class CelestialBody : MonoBehaviour
+    {
+        [SerializeField] BodyType cBody;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            App.Notify<BodyType>(cBody, new object[] { this });
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            App.Notify<BodyType>(cBody, new object[] { this });
+        }
+    }
+}
---
+++ Assets/MyGame/Scripts/PlanetReceivers/GenericReceiver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using com.vrglass.eventcontroller;
+using Gameplay.Sky;
+
+namespace Gameplay.Planet
+{
+    public abstract class GenericReceiver : EventController<BodyType>
+    {
+        protected CelestialBody activeBody;
+        protected float sunValue, moonValue;
+        
+        public override void NotifyController(BodyType notifyEvent, object[] parameters)
+        {
+            activeBody = parameters[0] as CelestialBody;
+        }
+
+        private void Update()
+        {
+            if (activeBody)
+            {
+                //TODO see body limits to send events
+            }
+        }
+    }
+}
